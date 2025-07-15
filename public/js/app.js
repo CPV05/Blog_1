4e7437e75d85f86f58c321510e759d20b02a16a0
CPV05 <carlespeve2005@gmail.com>
2025-07-15 13:23:28 +0200

    News css

--- public/js/app.js
+++ public/js/app.js
@@ -4,30 +4,26 @@ function renderPage(path)
 {
     appRoot.innerHTML = ''; // Clean the actual content
 
-    if (path === '/' || path === '/home') 
-    {
+    if (path === '/' || path === '/home'){
         appRoot.innerHTML = `<h1>Bienvenido a la página de Inicio</h1>
         <p>Contenido de la Home.</p>
         <a href="/about" class="nav-link">Acerca de Nosotros</a>`;
         document.title = 'Inicio';
     } 
-    else if (path === '/about') 
-    {
+    else if (path === '/about'){
         appRoot.innerHTML = `<h1>Acerca de Nosotros</h1>
         <p>Aquí puedes saber más sobre nosotros.</p>`;
         document.title = 'Sobre nosotros';
     }
-    else if (path === '/contact') 
-    {
+    else if (path === '/contact'){
         appRoot.innerHTML = `<h1>Contáctanos</h1>
         <p>Envíanos un mensaje.</p>`;
         document.title = 'Contacto';
     }
-    else
-    {
-        appRoot.innerHTML = `<h1 class="error center">404 - Página no encontrada</h1>
-        <p class="error center">Esta página no existe</p>
-        <a href="/home" class="error center">Vuelve al inicio</a>`;
+    else{
+        appRoot.innerHTML = `<h1 class="error center middle">404 - Página no encontrada</h1>
+        <p class="error center middle">Esta página no existe</p>
+        <a href="/home" class="error center middle">Vuelve al inicio</a>`;
         document.title = 'ERROR 404';
     }
 }
@@ -36,14 +32,12 @@ function handleNavClick(event)
 {
     const targetLink = event.target.closest('a.nav-link');
     
-    if (targetLink) 
-    {
+    if (targetLink) {
         event.preventDefault(); // Prevenir la recarga de la página
         const path = targetLink.getAttribute('href');
         
         // Si la ruta no es la actual, actualiza la URL y renderiza
-        if (window.location.pathname !== path) 
-        {
+        if (window.location.pathname !== path) {
             // Actualizar la URL en la barra del navegador
             window.history.pushState({}, '', path); 
             // Renderizar la nueva página
